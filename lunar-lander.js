aef593b00f9d207fc214ab362b36acb9fb7defad
Evellin-Miyamoto <117274609+Evellin-Miyamoto@users.noreply.github.com>
2023-03-03 22:15:07 +0100

    Set the velocity to land
    - Organised the code;
    - Changed the arrow up to spacebar due to the project requirements;
    - Defined the velocity to win, lose or bounce when landing.

--- lunar-lander.js
+++ lunar-lander.js
@@ -9,7 +9,13 @@ Evellin Miyamoto */
 2- win
 3- Lose */
 
+// Variables
 let screen = 0;
+let rocketX = 50;
+let rocketY = 50;
+let rocketVelocityY = 0.1;
+let rocketVelocityX = 0;
+let rocketAcceleration = 0.2;
 
 function setup() {
   createCanvas(800, 650);
@@ -37,7 +43,8 @@ function drawStatic() {
   rect(-10, 600, 800, 35, 10);
   fill(187, 208, 255);
   ellipse(50, 20, 100, 10);
-  ellipse(50, 540, 100, 100);
+  // ellipse(50, 540, 100, 100);
+  ellipse(50, 560, 100, 10);
   verticalObstacle(100, 160, 0);
   verticalObstacle(515, 100, 80);
   verticalObstacle(460, 275, 80);
@@ -64,29 +71,24 @@ function rocket(x, y) {
   ellipse(x, y - 15, 10, 10);
 }
 
-let rocketX = 50;
-let rocketY = 50;
-let rocketVelocityY = 0.1;
-let rocketVelocityX = 0;
-let rocketAcceleration = 0.2;
-
+// Redefine rocket position in the begining or every screen
 function rocketSetup() {
-  // Making the rocket move
   rocketX = 50;
   rocketY = 50;
   rocketVelocityY = 0.1;
   rocketVelocityX = 0;
-  rocketAcceleration = 0.1;
+  rocketAcceleration = 0.2;
 }
 
+// Making the rocket move
 function movingRocket() {
   rocket(rocketX, rocketY);
-
+  //console.log(rocketVelocityY);
   rocketVelocityY = rocketVelocityY + rocketAcceleration;
   rocketY = rocketY + rocketVelocityY;
   rocketX = rocketX + rocketVelocityX;
 
-  if (keyIsDown(38)) {
+  if (keyIsDown(32)) {
     rocketVelocityY = rocketVelocityY - 0.5;
   } else if (keyIsDown(39)) {
     rocketVelocityX = rocketVelocityX + 0.1;
@@ -114,17 +116,32 @@ function movingRocket() {
   } else if (rocketX > 0 && rocketX < 800 && rocketY > 600) {
     screen = 3;
   }
-  // Land on the portal - win
-  else if (rocketX > 5 && rocketX < 95 && rocketY > 560) {
-    screen = 2;
+
+  //Land on the portal
+  else if (rocketX > 5 && rocketX < 95 && rocketY > 530) {
+    // Lost because the rocket was too fast
+    if (rocketVelocityY > 3) {
+      screen = 3;
+    }
+    // Bounce
+    else if (rocketVelocityY > 0.01) {
+      rocketVelocityY = -rocketVelocityY * 0.95;
+    }
+    // Win because of zero speed
+    else {
+      screen = 2;
+      rocketY = 560;
+    }
   }
 }
+// The game mode with all mechanics
 function gameMode() {
   drawStatic();
   // rocketSetup();
   movingRocket();
 }
 
+// First Screen
 function startScreen() {
   background(255, 255, 255);
   fill(0, 0, 0);
@@ -132,15 +149,17 @@ function startScreen() {
   text("Land the Rocket!", 150, 200);
   push();
   textSize(25);
-  text("Use the up arrow to fight gravity", 180, 300);
+  text("Use the spacebar to fight gravity", 180, 300);
   text("Use the side arrows to swindle", 190, 360);
+  text("Be carefull with the landing velocity...", 180, 420);
   pop();
   push();
   textSize(40);
-  text("To start the game press enter", 110, 500);
+  text("To start the game press enter", 110, 520);
 }
 
-function gameWin() {
+// Screen won
+function gameWon() {
   background(255, 255, 255);
   fill(0, 0, 0);
   textSize(80);
@@ -152,6 +171,7 @@ function gameWin() {
   pop();
 }
 
+// Screen lost
 function gameLost() {
   background(0, 0, 0);
   fill(255, 255, 255);
@@ -164,7 +184,7 @@ function gameLost() {
   pop();
 }
 
-// different screens
+// Different screens
 /* inspired by these references
  https://editor.p5js.org/aferriss/sketches/rJwwjSvib
 https://www.youtube.com/watch?v=RlsRQS5qFSY */
@@ -175,18 +195,20 @@ function draw() {
   } else if (screen === 1) {
     gameMode();
   } else if (screen === 2) {
-    gameWin();
+    gameWon();
   } else if (screen === 3) {
     gameLost();
   }
 }
 
+// When the enter key is pressed on each screen
 function keyPressed() {
   if (screen === 0 && keyCode === 13) {
     screen = 1;
     gameMode();
   } else if (screen === 2 && keyCode === 13) {
     screen = 1;
+    rocketSetup();
     gameMode();
   } else if (screen === 3 && keyCode === 13) {
     screen = 1;
